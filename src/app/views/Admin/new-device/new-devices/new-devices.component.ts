d9821e554865a3ae9bbcebc0ead74d05b65fc373
Zied bousnina <91277905+Zied-bousnina@users.noreply.github.com>
2022-03-06 20:54:34 +0100

    Update new-devices.component.ts

--- src/app/views/Admin/new-device/new-devices/new-devices.component.ts
+++ src/app/views/Admin/new-device/new-devices/new-devices.component.ts
@@ -1,4 +1,8 @@
+import { DescriptionDevice } from './../../../../classes/description-device';
 import { Component, OnInit } from '@angular/core';
+import { Device } from 'src/app/classes/device';
+import { ServiceService } from '../../../../services/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-devices',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewDevicesComponent implements OnInit {
 
-  constructor() { }
+  newDevice = new DescriptionDevice()
+  dev = new Device()
+
+  constructor(private service:ServiceService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+  add(device:any, dev:any){
+    this.service.add(device,dev)
+    console.log(device)
+    console.log(dev)
+
+
   }
 
 }
